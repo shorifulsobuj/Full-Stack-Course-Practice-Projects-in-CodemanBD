6743089c0e2a616cc64ba0ce69e6c100a7749309
shorifulsobuj <sobujaiom126@gmail.com>
2023-12-06 23:45:21 +0600

    movie app

---
+++ MovieApp/tmbd/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  const movies = () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  };
+
+  return <></>;
+}
+
+export default App;
