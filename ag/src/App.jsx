f4a1c15b51f2fa23128747219a4859b47bf23b98
shorifulsobuj <sobujaiom126@gmail.com>
2024-01-08 22:24:18 +0600

    ag v1.0

---
+++ ag/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2
+        className="text-7xl text-center
+      text-red-500"
+      >
+        React js
+      </h2>
+    </>
+  );
+}
+
+export default App;
