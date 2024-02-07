0d21497fb348c5147b89c0140d466d2b17c579c8
shorifulsobuj <sobujaiom126@gmail.com>
2024-02-08 00:12:41 +0600

    v1.0

---
+++ megaCottage/src/components/MainLayout.jsx
@@ -0,0 +1,23 @@
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import "../styles/global.css";
+
+function MainLayout() {
+  return (
+    <>
+      <div className="mainLayout">
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="navbar">
+          <Header />
+        </div>
+        <main className="main p-8">
+          <h2>Test</h2>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default MainLayout;
---
+++ megaCottage/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+function Dashboard() {
+  return (
+    <>
+      <div>
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+          Dashboard
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
