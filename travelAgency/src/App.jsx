6923c6718283892f8709183fba987eae48566767
shorifulsobuj <sobujaiom126@gmail.com>
2024-03-20 23:00:18 +0600

    Travel Agency v0.3

--- travelAgency/src/App.jsx
+++ travelAgency/src/App.jsx
@@ -1,5 +1,24 @@
-import { Navbar, Hero, Footer, Memory, Explore } from "./components";
-import { hero, navlinks, memory, placesAPI } from "./data/travigodata";
+import React from "react";
+import {
+  Navbar,
+  Hero,
+  Footer,
+  Memory,
+  Explore,
+  Advertise,
+  Pricings,
+  Banner,
+  Newslatter,
+} from "./components";
+import {
+  hero,
+  navlinks,
+  memory,
+  placesAPI,
+  brands,
+  pricingapi,
+  footerAPI,
+} from "./data/travigodata";
 
 const App = () => {
   return (
@@ -8,7 +27,11 @@ const App = () => {
       <Hero hero={hero} />
       <Memory memory={memory} />
       <Explore title="Explore The Beauty of World" placesAPI={placesAPI} />
-      <Footer />
+      <Advertise brands={brands} />
+      <Pricings pricingapi={pricingapi} />
+      <Banner bannerAPI={bannerAPI} />
+      <Newslatter />
+      <Footer footerAPI={footerAPI} />
     </>
   );
 };
