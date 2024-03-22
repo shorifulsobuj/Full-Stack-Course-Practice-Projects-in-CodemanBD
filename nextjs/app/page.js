96e3f320e0606524e0fe1c7f157d72a53baf6b9b
shorifulsobuj <sobujaiom126@gmail.com>
2024-03-22 22:03:00 +0600

    NextJs v1.0

---
+++ nextjs/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="mt-10">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore amet rem
+        expedita quam obcaecati. Qui numquam, officiis vero quos libero magnam
+        repudiandae consequatur quisquam odio perferendis totam animi blanditiis
+        temporibus?
+      </div>
+    </main>
+  );
+}
