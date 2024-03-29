a8e159bce4e376e21a204cfea6d1b2453348308a
shorifulsobuj <sobujaiom126@gmail.com>
2024-03-29 22:39:42 +0600

    Cottage v2.0

--- megaCottage/src/pages/Dashboard.jsx
+++ megaCottage/src/pages/Dashboard.jsx
@@ -1,12 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-no-undef */
-import CreateRoomForm from "../features/Rooms/CreateRoomForm";
-
 function Dashboard() {
   return (
     <>
-      <div className="w-[800px]">
-        <CreateRoomForm />
+      <div>
+        <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+          Dashboard
+        </button>
       </div>
     </>
   );
